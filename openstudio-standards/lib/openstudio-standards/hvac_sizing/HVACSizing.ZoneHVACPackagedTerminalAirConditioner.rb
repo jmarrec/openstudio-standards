06755527ec1f9404fac4e33eb6e3d0d4d60aaac4
Julien Marrec <julien.marrec@gmail.com>
2016-02-10 15:33:44 +0100

    Created the methods to get autosized fields for ZoneHVACPackagedTerminalAirConditioner and to set them
    Originally did it last September here: https://github.com/NREL/OpenStudio-Prototype-Buildings/pull/63

--- openstudio-standards/lib/openstudio-standards/hvac_sizing/HVACSizing.ZoneHVACPackagedTerminalAirConditioner.rb
+++ openstudio-standards/lib/openstudio-standards/hvac_sizing/HVACSizing.ZoneHVACPackagedTerminalAirConditioner.rb
@@ -1,6 +1,47 @@
+# Author: Julien Marrec
 
 # open the class to add methods to return sizing values
 class OpenStudio::Model::ZoneHVACPackagedTerminalAirConditioner
+
+
+   # A helper method to get component sizes from the model
+  # returns the autosized value as an optional double
+  def getAutosizedValue(value_name, units)
+
+    result = OpenStudio::OptionalDouble.new
+
+    name = self.name.get.upcase
+    
+    object_type = self.iddObject.type.valueDescription.gsub('OS:','')
+    
+    # Get the model's attached sqlFile
+    sql = self.model.sqlFile
+    
+    if sql.is_initialized
+      sql = sql.get
+    
+      #SELECT * FROM ComponentSizes WHERE CompType = 'Coil:Heating:Gas' AND CompName = "COIL HEATING GAS 3" AND Description = "Design Size Nominal Capacity"
+      query = "SELECT Value 
+              FROM ComponentSizes 
+              WHERE CompType='#{object_type}' 
+              AND CompName='#{name}' 
+              AND Description='#{value_name}' 
+              AND Units='#{units}'"
+              
+      val = sql.execAndReturnFirstDouble(query)
+      
+      if val.is_initialized
+        result = OpenStudio::OptionalDouble.new(val.get)
+      else
+        #OpenStudio::logFree(OpenStudio::Warn, "openstudio.model.Model", "Data not found for query: #{query}")
+      end
+    else
+      OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', 'Model has no sql file containing results, cannot lookup data.')
+    end
+
+    return result
+  end   
+
 
   # Sets all auto-sizeable fields to autosize
   def autosize
@@ -12,16 +53,90 @@ class OpenStudio::Model::ZoneHVACPackagedTerminalAirConditioner
   # Must have previously completed a run with sql output for this to work.
   def applySizingValues
 
-    OpenStudio::logFree(OpenStudio::Warn, "openstudio.sizing.ZoneHVACPackagedTerminalAirConditioner", ".applySizingValues not yet implemented for #{self.iddObject.type.valueDescription}.")
+    # Supply Air Flow Rate During Cooling Operation
+    supply_air_flow_cooling = self.autosizedSupplyAirFlowRateDuringCoolingOperation
+    if supply_air_flow_cooling.is_initialized
+      self.setSupplyAirFlowRateDuringCoolingOperation(supply_air_flow_cooling.get)
+    end
+    
+    # Supply Air Flow Rate During Heating Operation
+    supply_air_flow_heating = self.autosizedSupplyAirFlowRateDuringHeatingOperation
+    if supply_air_flow_heating.is_initialized
+      self.setSupplyAirFlowRateDuringHeatingOperation(supply_air_flow_heating.get)
+    end
+    
+    # Supply Air Flow Rate When No Cooling or Heating is Needed
+    supply_air_flow_noload = self.autosizedSupplyAirFlowRateWhenNoCoolingorHeatingisNeeded
+    if supply_air_flow_noload.is_initialized
+      self.setSupplyAirFlowRateWhenNoCoolingorHeatingisNeeded(supply_air_flow_noload.get)
+    end
+    
+    # Outdoor Air Flow Rate During Cooling Operation
+    oa_air_flow_cooling = self.autosizedOutdoorAirFlowRateDuringCoolingOperation
+    if oa_air_flow_cooling.is_initialized
+      self.setOutdoorAirFlowRateDuringCoolingOperation(oa_air_flow_cooling.get)
+    end
+    
+    # Outdoor Air Flow Rate During Heating Operation
+    oa_air_flow_heating    = self.autosizedOutdoorAirFlowRateDuringHeatingOperation
+    if oa_air_flow_heating.is_initialized
+      self.setOutdoorAirFlowRateDuringHeatingOperation(oa_air_flow_heating.get)
+    end
+    
+    # Outdoor Air Flow Rate When No Cooling or Heating is Needed
+    oa_air_flow_noload = self.autosizedOutdoorAirFlowRateWhenNoCoolingorHeatingisNeeded
+    if oa_air_flow_noload.is_initialized
+      self.setOutdoorAirFlowRateWhenNoCoolingorHeatingisNeeded(oa_air_flow_noload.get)
+    end
+    
         
   end
 
-  # returns the autosized design supply air flow rate as an optional double
-  # def autosizedDesignSupplyAirFlowRate
 
-    # return self.model.getAutosizedValue(self, 'Design Supply Air Flow Rate', 'm3/s')
-    
-  # end
+  # Supply Air Flow Rate During Cooling Operation
+  def autosizedSupplyAirFlowRateDuringCoolingOperation
   
+    return self.getAutosizedValue('Design Size Cooling Supply Air Flow Rate', 'm3/s')
+  
+  end
+  
+  # Supply Air Flow Rate During Heating Operation
+  def autosizedSupplyAirFlowRateDuringHeatingOperation
+  
+    return self.getAutosizedValue('Design Size Heating Supply Air Flow Rate', 'm3/s')
+  
+  end
+  
+  # Supply Air Flow Rate When No Cooling or Heating is Needed
+  def autosizedSupplyAirFlowRateWhenNoCoolingorHeatingisNeeded
+  
+    return self.getAutosizedValue('Design Size No Load Supply Air Flow Rate', 'm3/s')
+  
+  end
+  
+  # Outdoor Air Flow Rate During Cooling Operation
+  def autosizedOutdoorAirFlowRateDuringCoolingOperation
+  
+    return self.getAutosizedValue('Design Size Outdoor Air Flow Rate During Cooling Operation', 'm3/s')
+  
+  end
+
+  # Outdoor Air Flow Rate During Heating Operation
+  def autosizedOutdoorAirFlowRateDuringHeatingOperation
+  
+    return self.getAutosizedValue('Design Size Outdoor Air Flow Rate During Heating Operation', 'm3/s')
+  
+  end
+  
+  # Outdoor Air Flow Rate When No Cooling or Heating is Needed
+  def autosizedOutdoorAirFlowRateWhenNoCoolingorHeatingisNeeded
+  
+    return self.getAutosizedValue('Design Size Outdoor Air Flow Rate When No Cooling or Heating is Needed', 'm3/s')
+  
+  end
+  
+  
+ 
   
 end
+
